15c2385135021197045b6ed05ccc496d52d1de3d
Jean-François Marronnier <jfmarronnier@epitez.fr>
2014-02-17 11:38:34 +0100

    Create kata environment

---
+++ reversi.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestReversiGame(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_todo(self):
+        # todo: write assertion here
+        self.fail("Test to be written")
+
+if __name__ == '__main__':
+    unittest.main()
